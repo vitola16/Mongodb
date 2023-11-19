aabc413b32ca0e4073d0bcb5f29db3f17fc2a195
Jose  David Vitola Beltran <vitolajose95@gmail.com>
2023-11-19 10:17:57 -0500

    formulario

--- src/routes/principal.routes.js
+++ src/routes/principal.routes.js
@@ -16,6 +16,6 @@ const { isAuthenticated } = require("../helpers/auth");
 router.post("/Guardarsensor", guardarsensor);
 router.get("/Registrarsensor", isAuthenticated,registrarsensor);
 router.get("/Mostrarsensores", isAuthenticated, listarsensores);
-router.post("/telematica/postiot",postValorIoT);
+
 
 module.exports = router;
